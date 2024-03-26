dced0b96a29db09637942b77a4b808a8e5fe3786
Stephen Ochieng <materiamystic@gmail.com>
2024-03-26 11:13:49 +0300

    Adds required solution

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function superbowlWin(arr) {
+  let found = arr.find((item) => item.result === "W");
+  if (found) {
+    return found.year;
+  }
+}
+
+// const record = [
+//   { year: "2015", result: "W" },
+//   { year: "2014", result: "N/A" },
+//   { year: "2013", result: "L" },
+//   //...
+// ];
